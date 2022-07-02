4619cb4a101f711b28d6a40b4d538f93d24c7bbb
Vickey <vickey21299@iiitd.ac.in>
2022-07-02 13:36:56 +0530

    second update

--- assembler.py
+++ assembler.py
@@ -1,9 +1,12 @@
+# co project starting 
+# group 
+from operator import invert
 import sys
 message  = sys.stdin.read()
-opcode ={"add":"10000",
+op ={"add":"10000",
         "sub":"10001",
-        "mov":"10010",
-        "mov":"10011",
+        "mov":("10010",
+        "10011"),
         "ld":"10100",
         "st":"10101",
         "mul":"10110",
@@ -37,36 +40,172 @@ def extractDigits(k):
       
     return(res)
 k=list(message.split("\n"))
-print(k)
 inster=extractDigits(k)
-print(inster)
 def Add(a,b,c):
-    binary_list.append(opcode["add"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    binary_list.append(op["add"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
     return binary_list
 def sub(a,b,c):
-    binary_list.append(opcode["sub"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    binary_list.append(op["sub"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
     return binary_list
-flag = False
-error_line = 0
-error_msg = ''
-binary_list=[]
+def movimm(a,b):
+    Binary =bin(int(b[1:]))[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary)))+Binary
+        binary_list.append(op["mov"][0]+ty_reg[a]+Imm) # type B register
+        return binary_list
+    else:
+        binary_list.append(op["mov"][0]+ty_reg[a]+Binary)
+        return binary_list
+def movreg(a,b):
+    binary_list.append(op["mov"][1]+"00000" + ty_reg[a]+ty_reg[b]) # type C register
+    return binary_list
+def load(a,b):
+    Binary =bin(b)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["ld"]+ty_reg[a]+Imm) # type B register
+        return binary_list
+    else:
+        binary_list.append(op["ld"]+ty_reg[a]+Binary)
+        return binary_list
+def store(a,b):
+    Binary =bin(b)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["st"]+ty_reg[a]+Imm) # type B register
+        return binary_list
+    else:
+        binary_list.append(op["st"]+ty_reg[a]+Binary)
+        return binary_list
+def  multiply(a,b,c):
+    binary_list.append(op["mul"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    return binary_list
+def divide(a,b):
+    binary_list.append(op["div"]+"00000" + ty_reg[a]+ty_reg[b]) # type C register
+    return binary_list
+def Rightshift(a,b):
+    Binary =bin(int(b[1:])[2:])
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["st"]+ty_reg[a]+Imm) # type B register
+        return binary_list
+def leftshift(a,b):
+    Binary =bin(int(b[1:])[2:])
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["st"]+ty_reg[a]+Imm) # type B register
+        return binary_list
+def exclusive_or(a,b,c):
+    binary_list.append(op["xor"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    return binary_list
+def Or(a,b,c):
+    binary_list.append(op["or"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    return binary_list
+def And(a,b,c):
+    binary_list.append(op["and"]+"00"+ty_reg[a]+ty_reg[b]+ty_reg[c]) # type A format of register
+    return binary_list
+def Invert(a,b):
+    binary_list.append(op["not"]+"00000" + ty_reg[a]+ty_reg[b]) # type C register
+    return binary_list
+def campare(a,b):
+    binary_list.append(op["cmp"]+"00000" + ty_reg[a]+ty_reg[b]) # type C register
+    return binary_list
+def uncontroljump(a):
+    Binary =bin(a)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["jmp"]+"000"+Imm) # type E register
+        return binary_list
+def jumpless(a):
+    Binary =bin(a)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["jmp"]+"000"+Imm) # type E register
+        return binary_list
+def jumphigh(a):
+    Binary =bin(a)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["jmp"]+"000"+Imm) # type E register
+        return binary_list
+def jumpequal(a):
+    Binary =bin(a)[2:]
+    if(len(Binary)<8):
+        Imm=str("0"*(8-len(Binary))+Binary)
+        binary_list.append(op["jmp"]+"000"+Imm) # type E register
+        return binary_list 
+def Halt():
+    binary_list.append(op["hlt"]+"00000000000")
+    return binary_list
 
-last_var = False
-##Number of variables declared
+binary_list=[]
 line_count = 0
 for x in inster:
     if x==[]:
         line_count+=1
-    if x[0] in opcode:
-        # checking opcode is maching or not 
+    if x!=[] and x[0] in op:
+        # checking opcode  is maching or not 
         if x[0]=="add":
             line_count+=1
             Add(x[1],x[2],x[3])
         if x[0]=="sub":
             line_count+=1
             sub(x[1],x[2],x[3])
-        if x[0]=="mov"
+        if x[0]=="mov" and x[2][0]=='$':
+            line_count+=1
+            movimm(x[1],x[2])
+        if x[0]=="mov" and x[2] in ty_reg:
+            line_count+=1
+            movreg(x[1],x[2])
+        if x[0]=="ld":
+            line_count+=1
+            load(x[1],x[2])
+        if x[0]=="st":
+            line_count+=1
+            store(x[1],x[2])
+        if x[0]=="mul":
+            line_count+=1
+            multiply(x[1],x[2],x[3])
+        if x[0]=="div":
+            line_count+=1
+            divide(x[1],x[2],x[3])
+        if x[0]=="rs":
+            line_count+=1
+            Rightshift(x[1],x[2])
+        if x[0]=="lf":
+            line_count+=1
+            leftshift(x[1],x[2])
+        if x[0]=="xor":
+            line_count+=1
+            exclusive_or(x[1],x[2],x[3])
+        if x[0]=="or":
+            line_count+=1
+            Or(x[1],x[2],x[3])
+        if x[0]=="and":
+            line_count+=1
+            And(x[1],x[2],x[3])
+        if x[0]=="not":
+            line_count+=1
+            invert(x[1],x[2])
+        if x[0]=="cmp":
+            line_count+=1
+            campare(x[1],x[2],x[3])
+        if x[0]=="jmp":
+            line_count+=1
+            uncontroljump(x[1])
+        if x[0]=="jlt":
+            line_count+=1
+            jumpless(x[1])
 
-
-             
+        if x[0]=="jgt":
+            line_count+=1
+            jumphigh(x[1],x[2],x[3])
+        if x[0]=="je":
+            line_count+=1
+            jumpequal(x[1],x[2],x[3])
+        if x[0]=="hlt":
+            line_count+=1
+            Halt()
+print(binary_list)      
+        
 
